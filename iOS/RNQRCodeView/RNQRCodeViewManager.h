7ce7e043453d00aa06e261437e0a6c2858357e33
KelseyRegan <kjregan10@gmail.com>
2015-11-25 14:45:41 -0800

    adds drawing

---
+++ iOS/RNQRCodeView/RNQRCodeViewManager.h
@@ -0,0 +1,15 @@
+//
+//  RNQRCodeViewManager.h
+//  RNQRCodeView
+//
+//  Created by Kelsey Regan on 2015-11-25.
+//  Copyright © 2015 Elevated Pixels. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "RCTViewManager.h"
+
+@interface RNQRCodeViewManager : RCTViewManager
+
+@end
---
+++ index.js
@@ -0,0 +1,31 @@
+{
+  "name": "react-native-qrcode-view",
+  "version": "0.1.0",
+  "description": "This view takes qrcode data and displays it in a simple, optimized view.",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/KelseyRegan/react-native-qrcode-view.git"
+  },
+  "keywords": [
+    "react",
+    "native",
+    "qrcode",
+    "rn",
+    "ios",
+    "android",
+    "view"
+  ],
+  "author": "Kelsey Regan",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/KelseyRegan/react-native-qrcode-view/issues"
+  },
+  "homepage": "https://github.com/KelseyRegan/react-native-qrcode-view#readme",
+  "dependencies": {
+    "react-native": "^0.15.0"
+  }
+}
