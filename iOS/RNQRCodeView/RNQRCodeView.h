99ac71a53dbdf637622477b7346ea4a118853c10
KelseyRegan <kjregan10@gmail.com>
2015-11-25 13:00:37 -0800

    merg

---
+++ iOS/RNQRCodeView/RNQRCodeView.h
@@ -0,0 +1,13 @@
+//
+//  RNQRCodeView.h
+//  RNQRCodeView
+//
+//  Created by Kelsey Regan on 2015-11-24.
+//  Copyright © 2015 Elevated Pixels. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNQRCodeView : NSObject
+
+@end
---
+++ iOS/RNQRCodeView/RNQRCodeView.m
@@ -0,0 +1,13 @@
+//
+//  RNQRCodeView.m
+//  RNQRCodeView
+//
+//  Created by Kelsey Regan on 2015-11-24.
+//  Copyright © 2015 Elevated Pixels. All rights reserved.
+//
+
+#import "RNQRCodeView.h"
+
+@implementation RNQRCodeView
+
+@end
